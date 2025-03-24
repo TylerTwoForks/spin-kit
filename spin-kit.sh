4ef87b6902b36647035cc70306992abac75b852a
TylerTwoForks <tyler.g.edmonds@gmail.com>
2025-03-24 11:28:22 -0400

    streamlining and fixing some bugs

--- spin-kit.sh
+++ spin-kit.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 ## customize these values #############################
-sandbox_list=("zincTe2Dev" "update_me2" "update_me3")
+sandbox_list=("update_me1" "update_me2" "update_me3")
 prod_alias="Prod"
 #######################################################
 
 index=${#sandbox_list[@]}
 trap 'menu' ERR
 
-enterChoice(){
+enterChoice() {
+    unset $choice
+    unset $sandbox_name
     read -p "Enter your choice: " choice
 }
 
@@ -21,7 +23,7 @@ calculateWidth() {
             max_length=$length
         fi
     done
-    width=$((max_length + 30))
+    width=$((max_length + 22))
 }
 
 menu() {
@@ -29,65 +31,101 @@ menu() {
     echo "┌$(printf '─%.0s' $(seq 1 $width))┐"
     echo "│$(printf ' %.0s' $(seq 1 $((width / 2 - 5))))MAIN MENU$(printf ' %.0s' $(seq 1 $((width / 2 - 4))))│"
     echo "├$(printf '─%.0s' $(seq 1 $width))┤"
+    echo "│ Refresh $(printf ' %.0s' $(seq 1 $((width - 9))))│"
     for i in "${!sandbox_list[@]}"; do
-        printf "│ %d: Refresh %s %$(($width - ${#sandbox_list[$i]} - 13))s│\n" "$i" "${sandbox_list[$i]}" ""
-        echo "├$(printf '─%.0s' $(seq 1 $width))┤"
+        printf "│  %d: %s %$(($width - ${#sandbox_list[$i]} - 6))s│\n" "$i" "${sandbox_list[$i]}" ""
     done
-    echo "│ l | ls: List Org Connections $(printf ' %.0s' $(seq 1 $((width - 30))))│"
-    echo "├$(printf '─%.0s' $(seq 1 $width))┤"
-    echo "│ c | custom: Refresh Custom Sandbox $(printf ' %.0s' $(seq 1 $((width - 36))))│"
-    echo "├$(printf '─%.0s' $(seq 1 $width))┤"
-    echo "│ m | menu: Show menu $(printf ' %.0s' $(seq 1 $((width - 21))))│"
-    echo "├$(printf '─%.0s' $(seq 1 $width))┤"
-    echo "│ x | exit: Exit $(printf ' %.0s' $(seq 1 $((width - 16))))│"
-    echo "└$(printf '─%.0s' $(seq 1 $width))┘"
+    echo "│ $(printf ' %.0s' $(seq 1 $((width - 1))))│"
+    echo "│ Utilities $(printf ' %.0s' $(seq 1 $((width - 11))))│"
+    echo -e "│  ls:\tList Org Connections $(printf ' %.0s' $(seq 1 $((width - 28))))│"
+    echo -e "│  c:\tRefresh Custom Sandbox $(printf ' %.0s' $(seq 1 $((width - 30))))│"
+    echo -e "│  rc:\tReconnect to Sandbox $(printf ' %.0s' $(seq 1 $((width - 28))))│"
+    echo -e "│  m:\tShow menu $(printf ' %.0s' $(seq 1 $((width - 17))))│"
+    echo -e "│  x:\tExit $(printf ' %.0s' $(seq 1 $((width - 12))))│"
+    echo -e "└$(printf '─%.0s' $(seq 1 $width))┘"
 
     enterChoice
 }
 
-orgStatus(){
-    echo "Org Connections:";
-    sf org list;
+orgStatus() {
+    echo "Org Connections:"
+    sf org list
     enterChoice
 }
 
-
-
-menu
-while true; do
-
-# dynamic choices
-    if [[ "$choice" =~ ^[0-9]+$ ]] && [ "$choice" -ge 0 ] && [ "$choice" -lt "$index" ]; then
-        echo "Refreshing ${sandbox_list[$choice]}"
-        sf org refresh sandbox --name ${sandbox_list[$choice]} --target-org $prod_alias
-        # continue
+isValidChoice() {
+    if [[ "$sandbox_name" == "" ]]; then
+        echo "${sandbox_name} is empty, please try again."
+        unset $sandbox_name
+        return 1
+    elif [[ "$sandbox_name" == "exit" || "$sandbox_name" == "x" ]]; then
+        unset $sandbox_name
+        echo "returning to menu..."
+        myApp
+        return 0
+    else
+        return 0
     fi
+}
 
-# static choices
-    case $choice in
+myApp() {
+    unset $choice
+    unset $sandbox_name
+    menu
+    while true; do
+        # dynamic choices
+        if [[ "$choice" =~ ^[0-9]+$ ]] && [ "$choice" -ge 0 ] && [ "$choice" -lt "$index" ]; then
+            echo "Refreshing ${sandbox_list[$choice]}"
+            sf org refresh sandbox --name ${sandbox_list[$choice]} --target-org $prod_alias
+            # continue
+        fi
+
+        # static choices
+        case $choice in
         "")
             menu
             ;;
-        ls|list|l) 
+        ls)
             orgStatus
+        
             ;;
-        x)
+        x | exit)
             exit
             ;;
-        m|menu)
+        m)
             menu
             ;;
-        c|custom)
+        c)
             echo
             read -p "Enter Sandbox Name to Refresh: " sandbox_name
-            sf org refresh sandbox --name $sandbox_name --target-org $prod_alias
-            enterChoice
+            if ! isValidChoice; then
+                echo "Invalid choice, please try again."
+            else
+                echo "^c to return to menu or continue the login process in the browser"
+                sf org refresh sandbox --name $sandbox_name --target-org $prod_alias
+                
+            fi
+            ;;
+        rc)
+            echo
+            read -p "Enter Sandbox Name to Reconnect (or "x" to return to menu): " sandbox_name
+            if ! isValidChoice; then
+                echo "Invalid choice, please try again."
+            else
+                echo "^c to return to menu or continue the login process in the browser"
+                sf org web login --instance-url https://test.salesforce.com --alias $sandbox_name
+                
+            fi
+
             ;;
         *)
             echo "Invalid choice, please try again."
-            enterChoice
             ;;
-        
-    esac;
 
-done
+        esac
+        myApp
+
+    done
+}
+
+myApp
